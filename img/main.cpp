cb78fab0761da8eedaa858270ae7d879cf1a2b93
julia-azevedo <jap@ic.ufal.br>
2025-11-08 21:06:49 -0300

    feat: circuit build andvariables initialization

---
+++ img/main.cpp
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+
+
+const int trigPin = 9;
+const int echoPin = 10;
+const int dcPin = 11;
+const float totalHeight = 103; // total height of bottle in cm
+
+const int rsPin = 13;
+const int enablePin = 6;
+const int d4 = 5;
+const int d5 = 4;
+const int d6 = 3;
+const int d7 = 2;
+
+LiquidCrystal lcd(rsPin, enablePin, d4, d5, d6, d7);
+
+
+
+
